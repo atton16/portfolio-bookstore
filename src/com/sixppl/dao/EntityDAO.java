25b9b3e1f106568a898e4604489b30fb74da4a84
Monai Thangsuphanich <monai.thang@gmail.com>
2016-09-23 06:43:53 +1000

    Entity DAO Implementation

--- src/com/sixppl/dao/EntityDAO.java
+++ src/com/sixppl/dao/EntityDAO.java
@@ -1,10 +1,12 @@
 package com.sixppl.dao;
 
+import java.util.ArrayList;
+
 import com.sixppl.bean.EntityBean;
 
 public interface EntityDAO {
 	void insertEntity(EntityBean entity);
 	void updateEntity(EntityBean entity);
-	void deleteEntity(String ID);
-	EntityBean findEntity(String type, String keyword);	// Type = Publication/Author/School/Venue etc., Keyword = Caption
+	void deleteEntity(long ID);
+	ArrayList<EntityBean> findEntity(String type, String keyword);	// Type = Publication/Author/School/Venue etc., Keyword = Caption
 }
