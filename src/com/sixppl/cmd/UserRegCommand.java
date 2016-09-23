878c4f39219924c77d6a8fe048311d647f33d6cb
cloudpiece <zhangyuny@gmail.com>
2016-09-23 16:43:45 +1000

    change the DAO and corresponding command according to the new table

--- src/com/sixppl/cmd/UserRegCommand.java
+++ src/com/sixppl/cmd/UserRegCommand.java
@@ -1,6 +1,7 @@
 package com.sixppl.cmd;
 
 import java.io.IOException;
+import java.util.UUID;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -17,52 +18,65 @@ import com.sixppl.main.Application;
 
 public class UserRegCommand implements Command {
 	private UserDAO userDao;
+
 	public UserRegCommand() {
 		userDao = Application.getSharedInstance().getDAOFactory().getUserDAO();
 	}
+
+	@SuppressWarnings("null")
 	@Override
-	/*This method is for user registration*/
-	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	/* This method is for user registration */
+	public void execute(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		UserDTO user = new UserDTO();
-		String username,password,email;
+		String username, password, email, addr, cardno;
 		EmailSending emailSending = new EmailSending();
 		username = request.getParameter("username");
 		password = request.getParameter("password");
 		email = request.getParameter("email");
+		addr = request.getParameter("address");
+		cardno = request.getParameter("ccn");
 		if (username == null || username.equals("") || username == null
-				|| password == null || password.equals("") || email == null
-				|| email.equals("")){
+				|| password == null || password.equals("") || addr == null
+				|| addr.equals("") ||cardno == null
+						|| cardno.equals("") ) {
 			request.setAttribute("success", false);
-			request.setAttribute("error_msg", "The username or email or password should not be null.");
+			request.setAttribute("error_msg",
+					"The username or email or password should not be null.");
+			System.out.println("The username or email or password should not be null.");
+			return;
 		} else {
-			user = userDao.findUserByName(request.getParameter("username"));
-			if (user.getUsername() != null) {
+			//user = userDao.findUserByName(request.getParameter("username"));
+			if (userDao.findUserByName(request.getParameter("username")) != null) {
 				request.setAttribute("success", false);
 				request.setAttribute("error_msg", "The username has been used.");
-			} else {
-				user.setUsername(request.getParameter("username"));
-				password = BCrypt.hashpw(request.getParameter("password"),
-						BCrypt.gensalt());
-				user.setPassword(password);
-
-				user.setNickname(request.getParameter("nickname"));
-				user.setFirstname(request.getParameter("firstname"));
-				user.setLastname(request.getParameter("lastname"));
-				user.setEmail(request.getParameter("email"));
-				user.setBirthyear(request.getParameter("yob"));
-				user.setAddr(request.getParameter("address"));
-				user.setCardno(request.getParameter("ccn"));
-				userDao.addUser(user);
-				String to = request.getParameter("email");
-				String from = "asst2unsw@gmail.com";
-				String full_path = request.getRequestURL().toString();
-				emailSending.sendEmail(to, from, full_path + "/signup/confirm");
-			}
+				System.out.println("The username has been used");
+				return;
+			} 
+			user.setUsername(username);
+			password = BCrypt.hashpw(request.getParameter("password"),
+					BCrypt.gensalt());
+			user.setPassword(password);
+			user.setNickname(request.getParameter("nickname"));
+			user.setFirstname(request.getParameter("firstname"));
+			user.setLastname(request.getParameter("lastname"));
+			user.setEmail(request.getParameter("email"));
+			user.setBirthyear(request.getParameter("yob"));
+			user.setAddr(request.getParameter("address"));
+			user.setCardno(request.getParameter("ccn"));
+			String token = UUID.randomUUID().toString();
+			user.setTokenstring(token);
+			userDao.addUser(user);
+			//send email to register
+			
+			String to = request.getParameter("email");
+			String from = "asst2unsw@gmail.com";
+			String full_path = request.getRequestURL().toString();
+			emailSending.sendEmail(to, from, full_path + "/signup/confirm?token="+token);
+			
 		}
-		
-	}
 
-	
+	}
 
 }
