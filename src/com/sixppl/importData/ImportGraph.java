fd23550ea6551f4739d0126616159213dd1794fb
Monai Thangsuphanich <monai.thang@gmail.com>
2016-09-24 18:06:27 +1000

    Change import script into offline running

--- src/com/sixppl/importData/ImportGraph.java
+++ src/com/sixppl/importData/ImportGraph.java
@@ -1,193 +1,405 @@
 package com.sixppl.importData;
 
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-
-import com.sixppl.dao.support.EntityDAOImpl;
-import com.sixppl.dao.support.GraphDAOImpl;
 import com.sixppl.dto.EntityDTO;
 import com.sixppl.dto.GraphDTO;
 
 public class ImportGraph {
 
+	private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
+	private static final String DB_CONNECTION = "jdbc:mysql://localhost/bookstore";
+	private static final String DB_USER = "bookstore";
+	private static final String DB_PASSWORD = "F8ruehc2xCgRmmev";
+	
 	public ImportGraph() {
 		// TODO Auto-generated constructor stub
 	}
-
-	public static void main(String[] args) throws SQLException {
+	
+	public void dropEntityTable() throws SQLException {
+		String sql = "DROP TABLE IF EXISTS Entity";
+		Connection connection = null;
+		try {
+			connection = getDBConnection();
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.executeUpdate();
+			ps.close();
+		}
+		catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				}
+				catch (SQLException e) {}
+			}
+		}
+	}
+	
+	public void createEntityTable() throws SQLException {
+		String sql = "CREATE TABLE IF NOT EXISTS Entity ("
+				+ "ID BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,"
+				+ "EntityID text NOT NULL,"
+				+ "Class text NOT NULL,"
+				+ "Type text NOT NULL,"
+				+ "Caption text NOT NULL,"
+				+ "PRIMARY KEY(ID)"
+				+ ") ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;";
+		Connection connection = null;
+		try {
+			connection = getDBConnection();
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.executeUpdate();
+			ps.close();
+		}
+		catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				}
+				catch (SQLException e) {}
+			}
+		}
+	}
+	
+	public void insertEntity(EntityDTO entity) {
 		// TODO Auto-generated method stub
-		// 1. Read XML
-				NodeList nodeList = null;
-				String xmlPath = "/Users/monai/Documents/workspace/asst2_importgraph/res/dblp.xml";
-				InputSource xmlFile = new InputSource(xmlPath);
+		String sql = "INSERT INTO Entity (EntityID, Class, Type, Caption) VALUES(?,?,?,?)";
+		Connection connection = null;
+		try {
+			connection = getDBConnection();
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setString(1, entity.getEntityID());
+			ps.setString(2, entity.getEntityClass());
+			ps.setString(3, entity.getEntityType());
+			ps.setString(4, entity.getEntityCaption());
+			ps.executeUpdate();
+			ps.close();
+		}
+		catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		finally {
+			if (connection != null) {
 				try {
-					DocumentBuilderFactory builderFactory = DocumentBuilderFactory.newInstance();
-			    	DocumentBuilder builder = builderFactory.newDocumentBuilder();
-			        Document doc = builder.parse(xmlFile);
-					doc.getDocumentElement().normalize();
-					nodeList = doc.getElementsByTagName("*");
-					Node rootNode = nodeList.item(0);
-					nodeList = rootNode.getChildNodes();
-					// Remove non-element nodes
-					for(int i = nodeList.getLength()-1; i >= 0; i--){
-						if (nodeList.item(i).getNodeType() != Node.ELEMENT_NODE){
-							nodeList.item(i).getParentNode().removeChild(nodeList.item(i));
-						}
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
+					connection.close();
 				}
-				ArrayList<Integer> randomlist = new ArrayList<Integer>();
-				for(int i = 0; i < nodeList.getLength(); i++){
-					randomlist.add(i);
-				}
-				Collections.shuffle(randomlist);
-				// Retrieve 100 random publications
-				ArrayList<PublicationDTO> random_publications = new ArrayList<PublicationDTO>(); // search results
-				for (int i = 0; i < 100; i++){
-					PublicationHandler handler = new PublicationHandler();
-					random_publications.add(handler.toPublication(nodeList.item(randomlist.get(i))));
-				}
-			// 2. Drop existing Table
-				try {
-					EntityDAOImpl eDAO = new EntityDAOImpl();
-					GraphDAOImpl gDAO = new GraphDAOImpl();
-					eDAO.dropTable();
-					gDAO.dropTable();
-				} catch (SQLException e) {
-					System.out.println(e.getMessage());
-				}
-			// 3. Create Table
-				try {
-					EntityDAOImpl eDAO = new EntityDAOImpl();
-					GraphDAOImpl gDAO = new GraphDAOImpl();
-					eDAO.createTable();
-					gDAO.createTable();
-				} catch (SQLException e) {
-					System.out.println(e.getMessage());
-				}
-			// 4. Insert into database
-				long countPublication = 0;
-				long countAuthor = 0;
-				long countVenue = 0;
-				long countSchool = 0;
-				long countEdge = 0;
-				for (PublicationDTO p : random_publications) {
-					EntityDAOImpl eDAO = new EntityDAOImpl();
-					GraphDAOImpl gDAO = new GraphDAOImpl();
-					EntityDTO ep = new EntityDTO();
-					EntityDTO es = new EntityDTO();
-					EntityDTO ea = new EntityDTO();
-					EntityDTO ed = new EntityDTO();
-					EntityDTO ev = new EntityDTO();
-					EntityDTO ee = new EntityDTO();
-					GraphDTO pa = new GraphDTO();
-					GraphDTO pd = new GraphDTO();
-					GraphDTO as = new GraphDTO();
-					GraphDTO pv = new GraphDTO();
-					// Extract publication
-					ep.setEntityID("P"+ Long.toString(countPublication+1));
-					ep.setEntityClass("Node");
-					ep.setEntityType("Publication");
-					ep.setEntityCaption(p.getTitle());
-					eDAO.insertEntity(ep);
-					countPublication++;
-					// Extract School
-					if (p.getSchool() != null) {
-						
-						es.setEntityID("S" + Long.toString(countSchool+1));
-						es.setEntityClass("Node");
-						es.setEntityType("School");
-						es.setEntityCaption(p.getSchool());
-						eDAO.insertEntity(es);
-						countSchool++;
-					}
-					// Extract author
-					if (!p.getAuthor().isEmpty()){
-						for (String author : p.getAuthor()) {
-							ea.setEntityID("A" + Long.toString(countAuthor+1));
-							ea.setEntityClass("Node");
-							ea.setEntityType("Author");
-							ea.setEntityCaption(author);
-							eDAO.insertEntity(ea);
-							countAuthor++;
-							// Link Publication authored by Author
-							ee.setEntityID("E" + Long.toString(countEdge+1));
-							ee.setEntityClass("Edge");
-							ee.setEntityType("DirectedLink");
-							ee.setEntityCaption("authored by");
-							eDAO.insertEntity(ee);
-							pa.setNodeFrom(ep.getEntityID());
-							pa.setEdge(ee.getEntityID());
-							pa.setNodeTo(ea.getEntityID());
-							gDAO.insertGraph(pa);
-							countEdge++;
-							// Link Author affiliated in School 
-							if(p.getSchool() != null) {
-								ee.setEntityID("E" + Long.toString(countEdge+1));
-								ee.setEntityClass("Edge");
-								ee.setEntityType("DirectedLink");
-								ee.setEntityCaption("affiliated in");
-								eDAO.insertEntity(ee);
-								as.setNodeFrom(ea.getEntityID());
-								as.setEdge(ee.getEntityID());
-								as.setNodeTo(es.getEntityID());
-								gDAO.insertGraph(as);
-								countEdge++;
-							}
-						}
-					}
-					// Extract Editor
-					if (!p.getEditor().isEmpty()){
-						for (String editor : p.getEditor()) {
-							ed.setEntityID("A" + Long.toString(countAuthor+1));
-							ed.setEntityClass("Node");
-							ed.setEntityType("Author");
-							ed.setEntityCaption(editor);
-							eDAO.insertEntity(ed);
-							countAuthor++;
-							// Link Publication authored by Editor
-							ee.setEntityID("E" + Long.toString(countEdge+1));
-							ee.setEntityClass("Edge");
-							ee.setEntityType("DirectedLink");
-							ee.setEntityCaption("edited by");
-							eDAO.insertEntity(ee);
-							pd.setNodeFrom(ep.getEntityID());
-							pd.setEdge(ee.getEntityID());
-							pd.setNodeTo(ed.getEntityID());
-							gDAO.insertGraph(pd);
-							countEdge++;
-						}
-					}
-					// Extract Venue
-					if (p.getJournal() != null) {
-						ev.setEntityID("V" + Long.toString(countVenue+1));
-						ev.setEntityClass("Node");
-						ev.setEntityType("Venue");
-						ev.setEntityCaption(p.getJournal());
-						countVenue++;
-						// Link Publication published in Venue
-						ee.setEntityID("E" + Long.toString(countEdge+1));
-						ee.setEntityClass("Edge");
-						ee.setEntityType("DirectedLink");
-						ee.setEntityCaption("published in");
-						eDAO.insertEntity(ee);
-						pv.setNodeFrom(ep.getEntityID());
-						pv.setEdge(ee.getEntityID());
-						pv.setNodeTo(ev.getEntityID());
-						gDAO.insertGraph(pv);
-						countEdge++;
-					}
-				}
+				catch (SQLException e) {}
+			}
+		}
 	}
 
+	public void updateEntity(EntityDTO entity) {
+		// TODO Auto-generated method stub
+		String sql = "UPDATE Entity SET EntityID=?, Class=?, Type=?, Caption=? WHERE ID=?";
+		Connection connection = null;
+		try {
+			connection = getDBConnection();
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setString(1, entity.getEntityID());
+			ps.setString(2, entity.getEntityClass());
+			ps.setString(3, entity.getEntityType());
+			ps.setString(4, entity.getEntityCaption());
+			ps.setLong(5, entity.getID());
+			ps.executeUpdate();
+			ps.close();
+		}
+		catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				}
+				catch (SQLException e) {}
+			}
+		}
+	}
+
+	public void deleteEntity(long ID) {
+		// TODO Auto-generated method stub
+		String sql = "DELETE FROM Entity WHERE ID=?";
+		Connection connection = null;
+		try {
+			connection = getDBConnection();
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setLong(1, ID);
+			ps.executeUpdate();
+			ps.close();
+		}
+		catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				}
+				catch (SQLException e) {}
+			}
+		}
+	}
+
+	public ArrayList<EntityDTO> findEntity(String type, String keyword) {
+		// TODO Auto-generated method stub
+		ArrayList<EntityDTO> result = new ArrayList<EntityDTO>();
+		String sql = "SELECT * FROM Entity WHERE Type=? AND Caption LIKE ?";
+		if (keyword == null) {
+			sql = "SELECT * FROM Entity WHERE Type=?";
+		}
+		Connection connection = null;
+		try {
+			connection = getDBConnection();
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setString(1, type);
+			if (keyword != null) {
+				ps.setString(2, "%" + keyword + "%");
+			}
+			ResultSet rs = ps.executeQuery();
+			while ( rs.next() )
+		    {
+		      EntityDTO entity = new EntityDTO();
+		      entity.setID(rs.getLong("ID"));
+		      entity.setEntityID(rs.getString("EntityID"));
+		      entity.setEntityClass(rs.getString("Class"));
+		      entity.setEntityType(rs.getString("Type"));
+		      entity.setEntityCaption(rs.getString("Caption"));
+		      result.add(entity);
+		    }
+		    rs.close();
+		    ps.close();
+		}
+		catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				}
+				catch (SQLException e) {}
+			}
+		}
+		return result;
+	}
+
+	public ArrayList<String> findLinkedEntity(String node) {
+		// TODO Auto-generated method stub
+		ArrayList<String> result = new ArrayList<String>();
+		Connection connection = null;
+		try {
+			connection = getDBConnection();
+			CallableStatement cStmt = connection.prepareCall("{CALL ListNode(?)}");
+			cStmt.setString(1, node);
+			ResultSet rs = cStmt.executeQuery();
+			while ( rs.next() )
+		    {
+		      result.add(rs.getString("NodeID"));
+		    }
+		    rs.close();
+		    cStmt.close();
+		}
+		catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				}
+				catch (SQLException e) {}
+			}
+		}
+		return result;
+	}
+
+	public EntityDTO findEntityByEntityId(String entityID) {
+		// TODO Auto-generated method stub
+		EntityDTO result = new EntityDTO();
+		String sql = "SELECT * FROM Entity WHERE EntityID=?";
+		Connection connection = null;
+		try {
+			connection = getDBConnection();
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setString(1, entityID);
+			ResultSet rs = ps.executeQuery();
+			result.setID(rs.getLong("ID"));
+			result.setEntityID(rs.getString("EntityID"));
+			result.setEntityClass(rs.getString("Class"));
+			result.setEntityType(rs.getString("Type"));
+			result.setEntityCaption(rs.getString("Caption"));
+		    rs.close();
+		    ps.close();
+		}
+		catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				}
+				catch (SQLException e) {}
+			}
+		}
+		return result;
+	}
+	
+	public void dropGraphTable() throws SQLException {
+		String sql = "DROP TABLE IF EXISTS Graph";
+		Connection connection = null;
+		try {
+			connection = getDBConnection();
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.executeUpdate();
+			ps.close();
+		}
+		catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				}
+				catch (SQLException e) {}
+			}
+		}
+	}
+	
+	public void createGraphTable() throws SQLException {
+		String sql = "CREATE TABLE IF NOT EXISTS Graph ("
+				+ "ID BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,"
+				+ "NodeFrom text NOT NULL,"
+				+ "Edge text NOT NULL,"
+				+ "NodeTo text NOT NULL,"
+				+ "PRIMARY KEY(ID)"
+				+ ") ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;";
+		Connection connection = null;
+		try {
+			connection = getDBConnection();
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.executeUpdate();
+			ps.close();
+		}
+		catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				}
+				catch (SQLException e) {}
+			}
+		}
+	}
+	
+	public void insertGraph(GraphDTO graph) {
+		// TODO Auto-generated method stub
+		String sql = "INSERT INTO Graph (NodeFrom, Edge, NodeTo) VALUES(?,?,?)";
+		Connection connection = null;
+		try {
+			connection = getDBConnection();
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setString(1, graph.getNodeFrom());
+			ps.setString(2, graph.getEdge());
+			ps.setString(3, graph.getNodeTo());
+			ps.executeUpdate();
+			ps.close();
+		}
+		catch (SQLException e) {
+			System.out.println(e.getMessage());
+
+		}
+		finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				}
+				catch (SQLException e) {}
+			}
+		}
+	}
+	
+	public void updateGraph(GraphDTO graph) {
+		// TODO Auto-generated method stub
+		String sql = "UPDATE Graph SET NodeFrom=?, Edge=?, NodeTo=? WHERE ID=?";
+		Connection connection = null;
+		try {
+			connection = getDBConnection();
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setString(1, graph.getNodeFrom());
+			ps.setString(2, graph.getEdge());
+			ps.setString(3, graph.getNodeTo());
+			ps.setLong(4, graph.getID());
+			ps.executeUpdate();
+			ps.close();
+		}
+		catch (SQLException e) {
+			System.out.println(e.getMessage());
+
+		}
+		finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				}
+				catch (SQLException e) {}
+			}
+		}
+	}
+	
+	public void deleteGraph(long ID) {
+		// TODO Auto-generated method stub
+		String sql = "DELETE FROM Graph WHERE ID=?";
+		Connection connection = null;
+		try {
+			connection = getDBConnection();
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setLong(1, ID);
+			ps.executeUpdate();
+			ps.close();
+		}
+		catch (SQLException e) {
+			System.out.println(e.getMessage());
+
+		}
+		finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				}
+				catch (SQLException e) {}
+			}
+		}
+	}
+	
+	private static Connection getDBConnection() {
+		Connection dbConnection = null;
+		try {
+			Class.forName(DB_DRIVER);
+		} catch (ClassNotFoundException e) {
+			System.out.println(e.getMessage());
+		}
+		try {
+			dbConnection = DriverManager.getConnection(DB_CONNECTION, DB_USER,DB_PASSWORD);
+			return dbConnection;
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+		return dbConnection;
+	}
 }
