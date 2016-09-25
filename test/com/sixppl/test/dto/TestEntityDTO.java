1775b842bfb0cd66bc2204595b7b3a688575092d
Monai Thangsuphanich <monai.thang@gmail.com>
2016-09-26 00:29:26 +1000

    Rename test data and reorganise test script into separate folder

--- test/com/sixppl/test/dto/TestEntityDTO.java
+++ test/com/sixppl/test/dto/TestEntityDTO.java
@@ -1,4 +1,4 @@
-package com.sixppl.tests.dto;
+package com.sixppl.test.dto;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -16,6 +16,11 @@ import com.sixppl.dto.EntityDTO;
 
 public class TestEntityDTO {
 
+	ArrayList<EntityDTO> entityList;
+	Stack<EntityDTO> entityStack;
+	EntityDTO entity1;
+	EntityDTO entity2;
+	
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
 	}
@@ -26,6 +31,10 @@ public class TestEntityDTO {
 
 	@Before
 	public void setUp() throws Exception {
+		entityList = new ArrayList<EntityDTO>();
+		entityStack = new Stack<EntityDTO>();
+		entity1 = new EntityDTO(1, "P1", "Node", "Publication", "Article Title");
+		entity2 = new EntityDTO(2, "A1", "Node", "Author", "Firstname Lastname");
 	}
 
 	@After
@@ -34,96 +43,84 @@ public class TestEntityDTO {
 
 	@Test
 	public void testContainsIDinArrayList() {
-		ArrayList<EntityDTO> entityList = new ArrayList<EntityDTO>();
-		EntityDTO entity = new EntityDTO(1, "P1", "Node", "Publication", "Article Title");
-		EntityDTO entity2 = new EntityDTO(2, "A1", "Node", "Author", "Firstname Lastname");
 		// Expected false -> Should not found any matched ID in empty ArrayList
-		assertFalse(EntityDTO.containsID(entityList, entity.getID()));
+		assertFalse(EntityDTO.containsID(entityList, entity1.getID()));
 		assertFalse(EntityDTO.containsID(entityList, entity2.getID()));
-		entityList.add(entity);
+		entityList.add(entity1);
 		// Expected true -> Should found matched ID in ArrayList
-		assertTrue(EntityDTO.containsID(entityList, entity.getID()));
+		assertTrue(EntityDTO.containsID(entityList, entity1.getID()));
 		// Expected false -> Should not found any matched Entity of not inserted Entity
 		assertFalse(EntityDTO.containsID(entityList, entity2.getID()));
 		entityList.add(entity2);
 		// Expected true -> Should found matched of inserted Entity in ArrayList
-		assertTrue(EntityDTO.containsID(entityList, entity.getID()));
+		assertTrue(EntityDTO.containsID(entityList, entity1.getID()));
 		assertTrue(EntityDTO.containsID(entityList, entity2.getID()));
 		entityList.remove(0);
 		// Expected false -> Should not found any matched ID of removed Entity
-		assertFalse(EntityDTO.containsID(entityList, entity.getID()));
+		assertFalse(EntityDTO.containsID(entityList, entity1.getID()));
 		// Expected true -> Should found matched ID in ArrayList
 		assertTrue(EntityDTO.containsID(entityList, entity2.getID()));
 	}
 	
 	@Test
 	public void testContainsIDinStack() {
-		Stack<EntityDTO> entityStack = new Stack<EntityDTO>();
-		EntityDTO entity = new EntityDTO(1, "P1", "Node", "Publication", "Article Title");
-		EntityDTO entity2 = new EntityDTO(2, "A1", "Node", "Author", "Firstname Lastname");
 		// Expected false -> Should not found any matched ID in empty Stack
-		assertFalse(EntityDTO.containsID(entityStack, entity.getID()));
+		assertFalse(EntityDTO.containsID(entityStack, entity1.getID()));
 		assertFalse(EntityDTO.containsID(entityStack, entity2.getID()));
-		entityStack.push(entity);
+		entityStack.push(entity1);
 		// Expected true -> Should found matched of inserted Entity in Stack
-		assertTrue(EntityDTO.containsID(entityStack, entity.getID()));
+		assertTrue(EntityDTO.containsID(entityStack, entity1.getID()));
 		// Expected false -> Should not found any matched EntityID of not inserted Entity
 		assertFalse(EntityDTO.containsID(entityStack, entity2.getID()));
 		entityStack.push(entity2);
 		// Expected true -> Should found matched of inserted Entity in Stack
-		assertTrue(EntityDTO.containsID(entityStack, entity.getID()));
+		assertTrue(EntityDTO.containsID(entityStack, entity1.getID()));
 		assertTrue(EntityDTO.containsID(entityStack, entity2.getID()));
 		entityStack.pop();
 		// Expected true -> Should found matched of inserted Entity in Stack
-		assertTrue(EntityDTO.containsID(entityStack, entity.getID()));
+		assertTrue(EntityDTO.containsID(entityStack, entity1.getID()));
 		// Expected false -> Should not found any matched ID of removed Entity
 		assertFalse(EntityDTO.containsID(entityStack, entity2.getID()));
 	}
 
 	@Test
 	public void testContainsEntityIDinArrayList() {
-		ArrayList<EntityDTO> entityList = new ArrayList<EntityDTO>();
-		EntityDTO entity = new EntityDTO(1, "P1", "Node", "Publication", "Article Title");
-		EntityDTO entity2 = new EntityDTO(2, "A1", "Node", "Author", "Firstname Lastname");
 		// Expected false -> Should not found any matched EntityID in empty ArrayList
-		assertFalse(EntityDTO.containsEntityID(entityList, entity.getEntityID()));
+		assertFalse(EntityDTO.containsEntityID(entityList, entity1.getEntityID()));
 		assertFalse(EntityDTO.containsEntityID(entityList, entity2.getEntityID()));
-		entityList.add(entity);
+		entityList.add(entity1);
 		// Expected true -> Should found matched EntityID in ArrayList
-		assertTrue(EntityDTO.containsEntityID(entityList, entity.getEntityID()));
+		assertTrue(EntityDTO.containsEntityID(entityList, entity1.getEntityID()));
 		// Expected false -> Should not found any matched Entity of not inserted Entity
 		assertFalse(EntityDTO.containsEntityID(entityList, entity2.getEntityID()));
 		entityList.add(entity2);
 		// Expected true -> Should found matched of inserted Entity in ArrayList
-		assertTrue(EntityDTO.containsEntityID(entityList, entity.getEntityID()));
+		assertTrue(EntityDTO.containsEntityID(entityList, entity1.getEntityID()));
 		assertTrue(EntityDTO.containsEntityID(entityList, entity2.getEntityID()));
 		entityList.remove(0);
 		// Expected false -> Should not found any matched Entity of removed Entity
-		assertFalse(EntityDTO.containsEntityID(entityList, entity.getEntityID()));
+		assertFalse(EntityDTO.containsEntityID(entityList, entity1.getEntityID()));
 		// Expected true -> Should found matched Entity in ArrayList
 		assertTrue(EntityDTO.containsEntityID(entityList, entity2.getEntityID()));
 	}
 	
 	@Test
 	public void testContainsEntityIDinStack() {
-		Stack<EntityDTO> entityStack = new Stack<EntityDTO>();
-		EntityDTO entity = new EntityDTO(1, "P1", "Node", "Publication", "Article Title");
-		EntityDTO entity2 = new EntityDTO(2, "A1", "Node", "Author", "Firstname Lastname");
 		// Expected false -> Should not found any matched EntityID in empty Stack
-		assertFalse(EntityDTO.containsEntityID(entityStack, entity.getEntityID()));
+		assertFalse(EntityDTO.containsEntityID(entityStack, entity1.getEntityID()));
 		assertFalse(EntityDTO.containsEntityID(entityStack, entity2.getEntityID()));
-		entityStack.push(entity);
+		entityStack.push(entity1);
 		// Expected true -> Should found matched of inserted Entity in Stack
-		assertTrue(EntityDTO.containsEntityID(entityStack, entity.getEntityID()));
+		assertTrue(EntityDTO.containsEntityID(entityStack, entity1.getEntityID()));
 		// Expected false -> Should not found any matched EntityID of not inserted Entity
 		assertFalse(EntityDTO.containsEntityID(entityStack, entity2.getEntityID()));
 		entityStack.push(entity2);
 		// Expected true -> Should found matched of inserted Entity in Stack
-		assertTrue(EntityDTO.containsEntityID(entityStack, entity.getEntityID()));
+		assertTrue(EntityDTO.containsEntityID(entityStack, entity1.getEntityID()));
 		assertTrue(EntityDTO.containsEntityID(entityStack, entity2.getEntityID()));
 		entityStack.pop();
 		// Expected true -> Should found matched of inserted Entity in Stack
-		assertTrue(EntityDTO.containsEntityID(entityStack, entity.getEntityID()));
+		assertTrue(EntityDTO.containsEntityID(entityStack, entity1.getEntityID()));
 		// Expected false -> Should not found any matched EntityID of removed Stack
 		assertFalse(EntityDTO.containsEntityID(entityStack, entity2.getEntityID()));
 	}
