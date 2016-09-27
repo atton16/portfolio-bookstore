437d4862c611e43f37ec99a3f980d2f60117906d
cloudpiece <zhangyuny@gmail.com>
2016-09-27 10:58:39 +1000

    add logout command

---
+++ src/com/sixppl/cmd/UserLogoutCommand.java
@@ -0,0 +1,44 @@
+package com.sixppl.cmd;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+import com.sixppl.dao.SessionDAO;
+import com.sixppl.dao.UserDAO;
+import com.sixppl.dto.SessionDTO;
+import com.sixppl.dto.UserDTO;
+import com.sixppl.main.Application;
+
+public class UserLogoutCommand {
+	private UserDAO userDao;
+	private SessionDAO sessionDao;
+	public UserLogoutCommand() {
+		
+		sessionDao = Application.getSharedInstance().getDAOFactory().getSessionDAO();
+	}
+	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	
+
+		//String sessionId = request.getSession().getId();
+		String sessionId = "5642232";
+		if(sessionId == null || sessionId.equals("") )
+		{
+			request.setAttribute("success", false);
+			request.setAttribute("error_msg", "can not find sessionID");
+			return;
+		}
+		int sessionID = Integer.parseInt(sessionId);
+		SessionDTO session = new SessionDTO();
+		session.setSessionID(sessionID);
+		System.out.println("the session id is"+session.getSessionID()+"the userID is"+session.getUserID());
+		sessionDao.delSession(session);
+		request.setAttribute("success", true);
+	}
+
+}
