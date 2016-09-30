e2c75272d8a89e840e4fcb1ee974cddd037afc81
Attawit Kittikrairit <atton16@gmail.com>
2016-09-30 15:45:50 +1000

    - Cleanup dummy and foo

--- src/com/sixppl/dao/DAOFactory.java
+++ src/com/sixppl/dao/DAOFactory.java
@@ -5,95 +5,85 @@ import java.util.Map;
 import com.sixppl.dao.support.*;
 
 public class DAOFactory {
-	private static final String DUMMY_DAO = "dummyDAO";
-	private static final String FOO_DAO = "fooDAO";
-	private static final String USER_DAO = "userDAO";
-	private static final String SESSION_DAO = "sessionDAO";
-	private static final String ENTITY_DAO = "entityDAO";
-	private static final String GRAPH_DAO = "graphDAO";
-	private static final String CART_DAO = "cartDAO";
-	private static final String LISTING_DAO = "listingDAO";
-	private static final String ADMINUSER_DAO = "adminUserDAO";
-	private static final String ADMINUSERBAN_DAO = "adminUserBanDAO";
-	private static final String ADMINLOGIN_DAO = "adminLoginDAO";
-	private static final String ADMINANALYTICS_DAO = "adminAnalyticsDAO";
-	private static final String ADMINPUB_DAO = "adminPubDAO";
-	private static final String ADMINUSERACTIVITY_DAO = "adminUserActivityDAO";
+	private enum DAO {
+		USER,
+		SESSION,
+		ENTITY,
+		GRAPH,
+		CART,
+		LISTING,
+		ADMIN_USER,
+		ADMIN_USER_BAN,
+		ADMIN_LOGIN,
+		ADMIN_ANALYTICS,
+		ADMIN_PUB,
+		ADMIN_USER_ACTIVITY
+	}
 
-	private Map<String, Object> daos;
+	private Map<DAO, Object> daos;
 	
 	public DAOFactory() {
-		daos = new HashMap<String, Object>();
-		daos.put(DUMMY_DAO, new DummyDAOImpl());
-		daos.put(FOO_DAO, new FooDAOImpl());
-		daos.put(USER_DAO, new UserDAOImpl());
-		daos.put(SESSION_DAO, new SessionDAOImpl());
-		daos.put(ENTITY_DAO, new EntityDAOImpl());
-		daos.put(GRAPH_DAO, new GraphDAOImpl());
-		daos.put(CART_DAO, new CartDAOImpl());
-		daos.put(LISTING_DAO, new ListingDAOImpl());
-		daos.put(ADMINUSER_DAO, new AdminUserDAOImpl());
-		daos.put(ADMINUSERBAN_DAO, new AdminUserBanDAOImpl());
-		daos.put(ADMINLOGIN_DAO, new AdminLoginDAOImpl());
-		daos.put(ADMINANALYTICS_DAO, new AdminAnalyticsDAOImpl());
-		daos.put(ADMINPUB_DAO, new AdminPubDAOImpl());
-		daos.put(ADMINUSERACTIVITY_DAO, new AdminUserActivityDAOImpl());
+		daos = new HashMap<DAO, Object>();
+		daos.put(DAO.USER, new UserDAOImpl());
+		daos.put(DAO.SESSION, new SessionDAOImpl());
+		daos.put(DAO.ENTITY, new EntityDAOImpl());
+		daos.put(DAO.GRAPH, new GraphDAOImpl());
+		daos.put(DAO.CART, new CartDAOImpl());
+		daos.put(DAO.LISTING, new ListingDAOImpl());
+		daos.put(DAO.ADMIN_USER, new AdminUserDAOImpl());
+		daos.put(DAO.ADMIN_USER_BAN, new AdminUserBanDAOImpl());
+		daos.put(DAO.ADMIN_LOGIN, new AdminLoginDAOImpl());
+		daos.put(DAO.ADMIN_ANALYTICS, new AdminAnalyticsDAOImpl());
+		daos.put(DAO.ADMIN_PUB, new AdminPubDAOImpl());
+		daos.put(DAO.ADMIN_USER_ACTIVITY, new AdminUserActivityDAOImpl());
 
-	}
-	
-	public DummyDAO getDummyDAO() {
-		return (DummyDAO) daos.get(DUMMY_DAO);
-	}
-	
-	public FooDAO getFooDAO() {
-		return (FooDAO) daos.get(FOO_DAO);
 	}
 	
 	public UserDAO getUserDAO() {
-		return (UserDAO) daos.get(USER_DAO);
+		return (UserDAO) daos.get(DAO.USER);
 	}
 
 	public SessionDAO getSessionDAO(){
-		return (SessionDAO) daos.get(SESSION_DAO);
+		return (SessionDAO) daos.get(DAO.SESSION);
 	}
 
 	public EntityDAO getEntityDAO() {
-		return (EntityDAO) daos.get(ENTITY_DAO);
+		return (EntityDAO) daos.get(DAO.ENTITY);
 	}
 	
 	public GraphDAO getGraphDAO() {
-		return (GraphDAO) daos.get(GRAPH_DAO);
+		return (GraphDAO) daos.get(DAO.GRAPH);
 	}
 	
 	public CartDAO getCartDAO() {
-		return (CartDAO) daos.get(CART_DAO);
+		return (CartDAO) daos.get(DAO.CART);
 	}
 	
 	public ListingDAO getListingDAO() {
-		return (ListingDAO) daos.get(LISTING_DAO);
+		return (ListingDAO) daos.get(DAO.LISTING);
 	}
 	
 	public AdminUserDAO getAdminUserDAO() {
-		return (AdminUserDAO) daos.get(ADMINUSER_DAO);
+		return (AdminUserDAO) daos.get(DAO.ADMIN_USER);
 	}
 	
 	public AdminUserBanDAO getAdminUserBanDAO() {
-		return (AdminUserBanDAO) daos.get(ADMINUSERBAN_DAO);
+		return (AdminUserBanDAO) daos.get(DAO.ADMIN_USER_BAN);
 	}
 	
 	public AdminLoginDAO getAdminLoginDAO() {
-		return (AdminLoginDAO) daos.get(ADMINLOGIN_DAO);
+		return (AdminLoginDAO) daos.get(DAO.ADMIN_LOGIN);
 	}
 	
 	public AdminAnalyticsDAO getAdminAnalyticsDAO() {
-		return (AdminAnalyticsDAO) daos.get(ADMINANALYTICS_DAO);
+		return (AdminAnalyticsDAO) daos.get(DAO.ADMIN_ANALYTICS);
 	}
 	
 	public AdminPubDAO getAdminPubDAO() {
-		return (AdminPubDAO) daos.get(ADMINPUB_DAO);
+		return (AdminPubDAO) daos.get(DAO.ADMIN_PUB);
 	}
 	
 	public AdminUserActivityDAO getAdminUserActivityDAO() {
-		return (AdminUserActivityDAO) daos.get(ADMINUSERACTIVITY_DAO);
+		return (AdminUserActivityDAO) daos.get(DAO.ADMIN_USER_ACTIVITY);
 	}
 }
--- src/com/sixppl/dao/DummyDAO.java
+++
@@ -1,8 +0,0 @@
-package com.sixppl.dao;
-
-public interface DummyDAO {
-	/**
-	 * 
-	 */
-	String getPasswordHash(String username);
-}
